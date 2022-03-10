40ea73b4b5a43ae0c8fcfcc22f26eda34af319e6
Sougata Das <67154421+isougatadas@users.noreply.github.com>
2022-03-10 12:38:03 +0530

    Update EDA_App.py

--- EDA_App.py
+++ EDA_App.py
@@ -5,9 +5,9 @@ from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 st.set_page_config(page_title="EDA App", 
+page_icon="✏️", 
 layout="centered", 
-initial_sidebar_state="auto", 
-menu_items=None)
+initial_sidebar_state="auto")
 
 hide_st_style = """
             <style>
